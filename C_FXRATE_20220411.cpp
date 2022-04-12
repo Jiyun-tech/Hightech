357df83b01c07a25fad53930301780a69b785c81
Jiyun-tech <100850714+Jiyun-tech@users.noreply.github.com>
2022-04-12 09:23:12 +0900

    Update C_FXRATE_20220411.cpp

--- C_FXRATE_20220411.cpp
+++ C_FXRATE_20220411.cpp
@@ -9,6 +9,7 @@ int main () {
 	const float FXRATE_USD = 1233.5, FXRATE_JPY = 9.87, FXRATE_EUR = 1342.42, 
 				FXRATE_CNY = 193.21, FXRATE_GBP = 1604.97;
 	int inputKRW = 0; int inputNumber = 0; int returnKRW = 0; int returnKRWFinal = 0;
+	int returnKRWFinal2 = 0;
 	int returnKRW1000 = 0;
 	int returnKRW500 = 0;
 	int returnKRW100 = 0;
@@ -19,20 +20,23 @@ int main () {
 	scanf("%d", &inputKRW);
 	printf("====================\n");
 	
+	
 	// KRW -> USD
 	float outputUSD = inputKRW / FXRATE_USD;
 	int outputUSDFinal = outputUSD/1;
 	returnKRW = inputKRW - outputUSDFinal * FXRATE_USD; 
 	returnKRWFinal = returnKRW- returnKRW%10;
-	returnKRW1000 = returnKRWFinal/1000;
-	returnKRWFinal = returnKRWFinal - returnKRW1000 * 1000;
-	returnKRW500 = returnKRWFinal/500;
-	returnKRWFinal = returnKRWFinal - returnKRW500 * 500;
-	returnKRW100 = returnKRWFinal/100;
-	returnKRWFinal = returnKRWFinal - returnKRW100 * 100;
-	returnKRW50 = returnKRWFinal/50;
-	returnKRWFinal = returnKRWFinal - returnKRW50 * 50;
-	returnKRW10 = returnKRWFinal/10;
+	
+	returnKRWFinal2 = returnKRWFinal;
+	returnKRW1000 = returnKRWFinal2/1000;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW1000 * 1000;
+	returnKRW500 = returnKRWFinal2/500;
+	rreturnKRWFinal2 = returnKRWFinal2 - returnKRW500 * 500;
+	returnKRW100 = rreturnKRWFinal2/100;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW100 * 100;
+	returnKRW50 = returnKRWFinal2/50;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW50 * 50;
+	returnKRW10 = returnKRWFinal2/10;
 	printf("USD 기준 환율 : %10.2f\n", FXRATE_USD);
 	printf("환전 결과\n");
 	printf("달러 : %d 달러\n", outputUSDFinal);
@@ -40,20 +44,23 @@ int main () {
 			,returnKRWFinal, returnKRW1000, returnKRW500, returnKRW100, returnKRW50, returnKRW10);
 	printf("====================\n");
 
+	
 	// KRW -> JPY
 	float outputJPY = inputKRW / FXRATE_JPY;
 	int outputJPYFinal = outputJPY/1;
 	returnKRW = inputKRW - outputJPYFinal * FXRATE_JPY; 
 	returnKRWFinal = returnKRW- returnKRW%10;
-	returnKRW1000 = returnKRWFinal/1000;
-	returnKRWFinal = returnKRWFinal - returnKRW1000 * 1000;
-	returnKRW500 = returnKRWFinal/500;
-	returnKRWFinal = returnKRWFinal - returnKRW500 * 500;
-	returnKRW100 = returnKRWFinal/100;
-	returnKRWFinal = returnKRWFinal - returnKRW100 * 100;
-	returnKRW50 = returnKRWFinal/50;
-	returnKRWFinal = returnKRWFinal - returnKRW50 * 50;
-	returnKRW10 = returnKRWFinal/10;
+
+	returnKRWFinal2 = returnKRWFinal;
+	returnKRW1000 = returnKRWFinal2/1000;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW1000 * 1000;
+	returnKRW500 = returnKRWFinal2/500;
+	rreturnKRWFinal2 = returnKRWFinal2 - returnKRW500 * 500;
+	returnKRW100 = rreturnKRWFinal2/100;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW100 * 100;
+	returnKRW50 = returnKRWFinal2/50;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW50 * 50;
+	returnKRW10 = returnKRWFinal2/10;
 	printf("JPY 기준 환율 : %10.2f\n", FXRATE_JPY);
 	printf("환전 결과\n");
 	printf("엔 :  %d 엔\n", outputJPYFinal);
@@ -61,20 +68,23 @@ int main () {
 			,returnKRWFinal, returnKRW1000, returnKRW500, returnKRW100, returnKRW50, returnKRW10);
 	printf("====================\n");
 
+	
 	// KRW -> EUR
 	float outputEUR = inputKRW / FXRATE_EUR;
 	int outputEURFinal = outputEUR/1;
 	returnKRW = inputKRW - outputEURFinal * FXRATE_EUR; 
 	returnKRWFinal = returnKRW- returnKRW%10;
-	returnKRW1000 = returnKRWFinal/1000;
-	returnKRWFinal = returnKRWFinal - returnKRW1000 * 1000;
-	returnKRW500 = returnKRWFinal/500;
-	returnKRWFinal = returnKRWFinal - returnKRW500 * 500;
-	returnKRW100 = returnKRWFinal/100;
-	returnKRWFinal = returnKRWFinal - returnKRW100 * 100;
-	returnKRW50 = returnKRWFinal/50;
-	returnKRWFinal = returnKRWFinal - returnKRW50 * 50;
-	returnKRW10 = returnKRWFinal/10;
+	
+	returnKRWFinal2 = returnKRWFinal;
+	returnKRW1000 = returnKRWFinal2/1000;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW1000 * 1000;
+	returnKRW500 = returnKRWFinal2/500;
+	rreturnKRWFinal2 = returnKRWFinal2 - returnKRW500 * 500;
+	returnKRW100 = rreturnKRWFinal2/100;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW100 * 100;
+	returnKRW50 = returnKRWFinal2/50;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW50 * 50;
+	returnKRW10 = returnKRWFinal2/10;;
 	printf("EUR 기준 환율 : %10.2f\n", FXRATE_EUR);
 	printf("환전 결\n");
 	printf("유로 : %d 유로\n", outputEURFinal);
@@ -82,20 +92,23 @@ int main () {
 			,returnKRWFinal, returnKRW1000, returnKRW500, returnKRW100, returnKRW50, returnKRW10);
 	printf("====================\n");	 
 
+	
 	// KRW -> CNY
 	float outputCNY = inputKRW / FXRATE_CNY;
 	int outputCNYFinal = outputCNY/1;
 	returnKRW = inputKRW - outputCNYFinal * FXRATE_CNY; 
 	returnKRWFinal = returnKRW- returnKRW%10;
-	returnKRW1000 = returnKRWFinal/1000;
-	returnKRWFinal = returnKRWFinal - returnKRW1000 * 1000;
-	returnKRW500 = returnKRWFinal/500;
-	returnKRWFinal = returnKRWFinal - returnKRW500 * 500;
-	returnKRW100 = returnKRWFinal/100;
-	returnKRWFinal = returnKRWFinal - returnKRW100 * 100;
-	returnKRW50 = returnKRWFinal/50;
-	returnKRWFinal = returnKRWFinal - returnKRW50 * 50;
-	returnKRW10 = returnKRWFinal/10;
+	
+	returnKRWFinal2 = returnKRWFinal;
+	returnKRW1000 = returnKRWFinal2/1000;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW1000 * 1000;
+	returnKRW500 = returnKRWFinal2/500;
+	rreturnKRWFinal2 = returnKRWFinal2 - returnKRW500 * 500;
+	returnKRW100 = rreturnKRWFinal2/100;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW100 * 100;
+	returnKRW50 = returnKRWFinal2/50;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW50 * 50;
+	returnKRW10 = returnKRWFinal2/10;
 	printf("CNY 기준 환율 : %10.2f\n", FXRATE_CNY);
 	printf("환전 결과\n");
 	printf("위안 : %d 위안\n", outputCNYFinal);
@@ -103,20 +116,23 @@ int main () {
 			,returnKRWFinal, returnKRW1000, returnKRW500, returnKRW100, returnKRW50, returnKRW10);
 	printf("====================\n");
 
+	
 	// KRW -> GBP
 	float outputGBP = inputKRW / FXRATE_GBP;
 	int outputGBPFinal = outputGBP/1;
 	returnKRW = inputKRW - outputGBPFinal * FXRATE_GBP; 
 	returnKRWFinal = returnKRW - returnKRW%10;
-	returnKRW1000 = returnKRWFinal/1000;
-	returnKRWFinal = returnKRWFinal - returnKRW1000 * 1000;
-	returnKRW500 = returnKRWFinal/500;
-	returnKRWFinal = returnKRWFinal - returnKRW500 * 500;
-	returnKRW100 = returnKRWFinal/100;
-	returnKRWFinal = returnKRWFinal - returnKRW100 * 100;
-	returnKRW50 = returnKRWFinal/50;
-	returnKRWFinal = returnKRWFinal - returnKRW50 * 50;
-	returnKRW10 = returnKRWFinal/10;
+	
+	returnKRWFinal2 = returnKRWFinal;
+	returnKRW1000 = returnKRWFinal2/1000;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW1000 * 1000;
+	returnKRW500 = returnKRWFinal2/500;
+	rreturnKRWFinal2 = returnKRWFinal2 - returnKRW500 * 500;
+	returnKRW100 = rreturnKRWFinal2/100;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW100 * 100;
+	returnKRW50 = returnKRWFinal2/50;
+	returnKRWFinal2 = returnKRWFinal2 - returnKRW50 * 50;
+	returnKRW10 = returnKRWFinal2/10;
 	printf("GBP 기준 환율 : %10.2f\n", FXRATE_GBP);
 	printf("환전 결과\n");
 	printf("파운드 : %d 파운드\n", outputGBPFinal);
